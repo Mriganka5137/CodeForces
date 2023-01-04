acaf087b8384b3126ba09479edc5ca4814ae19be
Mriganka Gogoi <gogoimriganka09@gmail.com>
2023-01-04 15:50:40 +0530

    P151A solved

--- Implementation/P151A.cpp
+++ Implementation/P151A.cpp
@@ -5,6 +5,12 @@ int main()
 
   int n, k, l, c, d, p, nl, np;
   cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+  int t1 = (k * l) / nl;
+  int t2 = (c * d) / 1;
+  int t3 = p / np;
+
+  int toast = min(t1, min(t2, t3)) / n;
+  cout << toast;
 
   return 0;
 }
